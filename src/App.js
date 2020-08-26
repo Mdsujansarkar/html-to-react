9da5f574291c98153b61779811e45f5375d4aa17
Mdsujansarkar <sujansarkar786@gmail.com>
2020-08-26 09:15:17 +0600

    page add

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Gallery from './pages/Gallery';
+import Services from './pages/Services';
 import Footer from './components/Footer';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header/>
-    <Home/>
-    <Footer/>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/gallery' component={Gallery} />
+          <Route path='/services' component={Services} />
+        </Switch>
+        <Home />
+        <Footer />
+      </Router>
     </div>
   );
 }
